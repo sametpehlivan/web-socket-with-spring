8463e28beaca0b07d67a591136413f696329bc7f
Samet Pehlivan <samet16033@outlook.com>
2025-01-07 21:46:06 +0300

    first commit

---
+++ src/main/java/com/sametp/wsocket/WsocketApplication.java
@@ -0,0 +1,13 @@
+package com.sametp.wsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsocketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sametp/wsocket/WsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sametp.wsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
